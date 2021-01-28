0b39595f16b58a78c07f1413aa0a860529a3be34
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-01-27 18:32:55 -0800

    Update omni_g900em.mk

--- omni_g900em.mk
+++ omni_g900em.mk
@@ -1,38 +1,18 @@
-#
-# Copyright 2017 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Release name
 PRODUCT_RELEASE_NAME := g900em
 
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from hardware-specific part of the product configuration
+# Inherit from device
 $(call inherit-product, device/lge/g900em/device.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_BRAND := lge
 PRODUCT_DEVICE := g900em
 PRODUCT_NAME := omni_g900em
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG VELVET 5G
 PRODUCT_MANUFACTURER := LGE
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=caymanlm_lao_com \
-    BUILD_PRODUCT=caymanlm \
-    TARGET_DEVICE=caymanlm
+PRODUCT_MODEL := LG VELVET 5G
